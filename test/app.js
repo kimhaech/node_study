56019188e6d766c4b964aa08d34ad4317945b26e
kimhaech <epiclog@hufs.ac.kr>
2022-08-29 14:19:54 +0900

    get, post 및 모듈화 테스트

--- test/app.js
+++ test/app.js
@@ -7,8 +7,10 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+
 var app = express();
 
+app.use(express.json())
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -21,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- test/routes/index.js
+++ test/routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const userController = require('../controllers');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', userController.basicAPI);
+router.get('/test', userController.testAPI);
+router.post('/post_test', userController.postTestAPI);
 
 module.exports = router;
